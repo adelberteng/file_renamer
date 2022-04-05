a880de22f17d09c682c674c2f0308015e2549819
Albert <adelberteng@gmail.com>
2022-04-05 15:15:38 +0800

    feature: show  rename process.

--- main.go
+++ main.go
@@ -6,19 +6,28 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"flag"
 )
 
 func main() {
-	dirPath := "./sample/"
-	files, err := ioutil.ReadDir(dirPath)
+	dir := flag.String("dir", "", "files in directory need to rename.")
+	prefix := flag.String("prefix", "", "prefix of new file name.")
+	ext := flag.String("ext", "", "file extension.")
+	flag.Parse()
+
+	if *dir == "" {
+		log.Fatal("directory path is empty.")
+	}
+
+	files, err := ioutil.ReadDir(*dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	newFilePrefix := "new_"
-	fileExtension := ".txt"
+	filePrefix := *prefix + "_"
 	for i, f := range files {
-		fmt.Println(f.Name())
-		os.Rename(dirPath +f.Name(), dirPath + newFilePrefix + strconv.Itoa(i) + fileExtension)
+		fmt.Println(f.Name(), "to", filePrefix + strconv.Itoa(i) + "." + *ext)
+		os.Rename(*dir +f.Name(), *dir + filePrefix + strconv.Itoa(i) + *ext)
+		
 	}
 }
